7d46f6e7cfb3616cc35247bd78f5ce030277b41e
Lisa Fang <lisa.fang@brooklyndata.co>
2023-06-30 11:48:42 -0500

    streamlined flow file

--- .prefect/flow_build_project.py
+++ .prefect/flow_build_project.py
@@ -4,9 +4,9 @@ from prefect_dbt.cli.commands import DbtCoreOperation
 @flow
 def trigger_dbt_flow() -> str:
     result = DbtCoreOperation(
-        commands=["pwd", "dbt debug", "dbt run"],
+        commands=["dbt debug", "dbt run"],
         project_dir="../",
-        profiles_dir=".prefect" # copied my personal profile here
+        profiles_dir="." # copied my personal profile here
     ).run()
     return result
 
