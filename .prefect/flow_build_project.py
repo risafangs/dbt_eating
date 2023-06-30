576190fa19ec71f7885c0fe8885489ba27728a36
Lisa Fang <lisa.fang@brooklyndata.co>
2023-06-30 17:07:40 -0500

    add deployment

--- .prefect/flow_build_project.py
+++ .prefect/flow_build_project.py
@@ -1,14 +1,23 @@
 from prefect import flow
 from prefect_dbt.cli.commands import DbtCoreOperation
+from prefect.deployments import Deployment
+
+project_dir_path = "~/clients/internal/dbt_eating/"
 
 @flow
 def trigger_dbt_flow() -> str:
     result = DbtCoreOperation(
-        commands=["dbt debug", "dbt run"],
-        project_dir="../",
+        commands=["dbt debug", "dbt build"],
+        project_dir=project_dir_path,
         profiles_dir="." # copied my personal profile here
     ).run()
     return result
 
-trigger_dbt_flow()
+def deploy():
+    deployment = Deployment.build_from_flow(
+        flow=trigger_dbt_flow,
+        name="dbt-build-deployment"
+    )
+    deployment.apply()
 
+deploy()
